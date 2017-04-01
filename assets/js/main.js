2fde771379474cd88ab5c48e727bc3828472b083
Haider Ali Punjabi <haiderali@cyberservices.com>
2017-04-01 21:54:21 +0530

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -60,7 +60,7 @@
 				$(
 					'<div id="titleBar">' +
 						'<a href="#navPanel" class="toggle"></a>' +
-						'<span class="title">' + $('#logo').html() + '</span>' +
+						'<span class="title">' + "The Four Seasons" + '</span>' +
 					'</div>'
 				)
 					.appendTo($body);
