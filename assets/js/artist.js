4a1059490a0cac3d30ef0aa9dbd855c9b95a90e4
haideralipunjabi <haiderali@cyberservices.com>
2017-05-15 22:16:11 +0530

    AddedWorkWithUs

---
+++ assets/js/artist.js
@@ -0,0 +1,47 @@
+$(function() {
+  console.log('test');
+  $('#artist_type').on('change', function (e) {
+      console.log('change');
+      var optionSelected = $("option:selected", this);
+      var valueSelected = this.value;
+      if(valueSelected != '-1') {
+        $('#artist_type option[value="-1"]').remove();
+      }
+      if(valueSelected === 'other'){
+        $("#specify_other").css('display', 'block');
+      }
+  });
+  $('#specify_other').on('input',function(e){
+      $('#artist_type').value = $('#specify_other').value;
+     });
+ });
+var submitForm = function(){
+  
+  $.ajax({
+    url: 'http://127.0.0.1:8000/workwithus/artist',
+    type: 'POST',
+    crossDomain: true,
+    data: new FormData($("form")[0]),
+    cache: false,
+    contentType: false,
+    processData: false,
+    dataType: 'json',
+    success: function(json) {
+        showMessage("You have successfully registered as an Artist");
+      },
+    error: function(json){
+      showMessage("Registration Failed");
+    }
+  });
+  showMessage('Registering...');
+};
+
+
+
+
+
+var showMessage = function(message){
+  $message = $("#message");
+  $message.css('display', 'block');
+  $message.html(message);
+};
--- assets/js/volunteer.js
+++ assets/js/volunteer.js
@@ -1,31 +1,27 @@
 
 var submitForm = function(){
-  form = $("form")[0];
-  formData = {};
-  formData.volunteer_name = form.name.value;
-  formData.volunteer_photo = $('#photo')[0].files[0];
-  formData.address = form.address.value;
-  formData.country = form.country.selectedOptions[0].label;
-  formData.state = form.state.selectedOptions[0].label;
-  formData.pincode = form.pincode.value;
-  formData.contact_number = form.phone.value;
-  formData.volunteer_age = form.age.value;
-  formData.volunteer_dob = form.dob.value;
-  formData.volunteer_email = form.email.value;
-  
-  formData = JSON.stringify(formData);
-  console.log(formData);
+
   $.ajax({
     url: 'http://127.0.0.1:8000/workwithus/volunteer',
     type: 'POST',
     crossDomain: true,
-    data: formData,
+    data: new FormData($("form")[0]),
+    cache: false,
+    contentType: false,
+    processData: false,
     dataType: 'json',
     success: function(json) {
-        alert("You have successfully registered as a Volunteer");
+        showMessage("You have successfully registered as a Volunteer");
       },
     error: function(json){
-      alert("Registration Failed");
+      showMessage("Registration Failed");
     }
   });
+  showMessage('Registering...');
+};
+
+var showMessage = function(message){
+  $message = $("#message");
+  $message.css('display', 'block');
+  $message.html(message);
 };
