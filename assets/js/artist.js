9658ce9a50794308998a3b4c88a722a9081044fa
haideralipunjabi <haiderali@cyberservices.com>
2017-05-17 20:24:20 +0530

    UPDATEADDEDPDFGEN

--- assets/js/artist.js
+++ assets/js/artist.js
@@ -1,14 +1,15 @@
 $(function() {
-  console.log('test');
   $('#artist_type').on('change', function (e) {
-      console.log('change');
       var optionSelected = $("option:selected", this);
       var valueSelected = this.value;
+      $("#specify_other").css('display', 'none');
+      $("#artist_type_other").prop('required', false);
       if(valueSelected != '-1') {
         $('#artist_type option[value="-1"]').remove();
       }
       if(valueSelected === 'other'){
         $("#specify_other").css('display', 'block');
+        $("#artist_type_other").prop('required', true);
       }
   });
   $('#specify_other').on('input',function(e){
@@ -16,32 +17,55 @@ $(function() {
      });
  });
 var submitForm = function(){
+  console.log("submit");
+  if(validateForm($("form")[0])){
+        $.ajax({
+          url: 'http://127.0.0.1:8000/workwithus/artist',
+          type: 'POST',
+          crossDomain: true,
+          data: new FormData($("form")[0]),
+          cache: false,
+          contentType: false,
+          processData: false,
+          dataType: 'json',
+          success: function(json) {
+              showMessage("You have successfully registered as an Artist");
+            },
+          error: function(json){
+            showMessage("Registration Failed");
+          }
+        });
+        showMessage('Registering...');
+  }
+  };
+
+
+var validateForm = function(form){
+  //Add validation params here:
+  //Test 1: DOB format mathces dd-mm-yyyy
+  $dobRegEx = /^(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\d\d$/;
+  $emailRegEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if(!($dobRegEx.test($("#artist_dob")[0].value))){
+    showError('Date of Birth should be of dd-mm-yyyy format');
+    return false;
+  }
+  if(!($emailRegEx.test($("#artist_email")[0].value))){
+    showError('Invalid Email Input');
+    return false;
+  }
   
-  $.ajax({
-    url: 'http://fourseasons.pythonanywhere.com/workwithus/artist',
-    type: 'POST',
-    crossDomain: true,
-    data: new FormData($("form")[0]),
-    cache: false,
-    contentType: false,
-    processData: false,
-    dataType: 'json',
-    success: function(json) {
-        showMessage("You have successfully registered as an Artist");
-      },
-    error: function(json){
-      showMessage("Registration Failed");
-    }
-  });
-  showMessage('Registering...');
+    return true;
 };
-
-
-
 
 
 var showMessage = function(message){
   $message = $("#message");
   $message.css('display', 'block');
   $message.html(message);
+};
+var showError = function(message){
+  $message = $("#message_error");
+  $message.css('display', 'block');
+  $message.html(message);
+   window.location.href = "#page-wrapper";
 };
--- assets/js/volunteer.js
+++ assets/js/volunteer.js
@@ -1,27 +1,50 @@
 
 var submitForm = function(){
-
-  $.ajax({
-    url: 'http://localhost:8000/workwithus/volunteer',
-    type: 'POST',
-    crossDomain: true,
-    data: new FormData($("form")[0]),
-    cache: false,
-    contentType: false,
-    processData: false,
-    dataType: 'json',
-    success: function(json) {
-        showMessage("You have successfully registered as a Volunteer");
-      },
-    error: function(json){
-      showMessage("Registration Failed");
-    }
-  });
-  showMessage('Registering...');
+  if(validateForm())
+  {
+    $.ajax({
+      url: 'http://localhost:8000/workwithus/volunteer',
+      type: 'POST',
+      crossDomain: true,
+      data: new FormData($("form")[0]),
+      cache: false,
+      contentType: false,
+      processData: false,
+      dataType: 'json',
+      success: function(json) {
+          showMessage("You have successfully registered as a Volunteer");
+        },
+      error: function(json){
+        showMessage("Registration Failed");
+      }
+    });
+    showMessage('Registering...');
+  }
 };
-
+var validateForm = function(form){
+  //Add validation params here:
+  //Test 1: DOB format mathces dd-mm-yyyy
+  $dobRegEx = /^(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\d\d$/;
+  $emailRegEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if(!($dobRegEx.test($("#volunteer_dob")[0].value))){
+    showError('Date of Birth should be of dd-mm-yyyy format');
+    return false;
+  }
+  if(!($emailRegEx.test($("#volunteer_email")[0].value))){
+    showError('Invalid Email Input');
+    return false;
+  }
+  
+    return true;
+};
 var showMessage = function(message){
   $message = $("#message");
   $message.css('display', 'block');
   $message.html(message);
+};
+var showError = function(message){
+  $message = $("#message_error");
+  $message.css('display', 'block');
+  $message.html(message);
+   window.location.href = "#page-wrapper";
 };
